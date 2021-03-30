d3f98b077a40bc4ee9b42689df33feec62cdfbca
Matt Bierner <matb@microsoft.com>
2021-03-30 22:01:49 +0000

    Allow inline matbh blocks
    Pulls in https://github.com/iktakahiro/markdown-it-katex/pull/2

--- index.js
+++ index.js
@@ -152,6 +152,68 @@ function math_block(state, start, end, silent){
     return true;
 }
 
+function math_inline_block(state, silent) {
+    var start, match, token, res, pos, esc_count;
+
+    if(state.src.slice(state.pos, state.pos+2) !== "$$") { return false; }
+
+    res = isValidDelim(state, state.pos + 1);
+    if (!res.can_open) {
+        if (!silent) { state.pending += "$$"; }
+        state.pos += 2;
+        return true;
+    }
+
+    // First check for and bypass all properly escaped delimieters
+    // This loop will assume that the first leading backtick can not
+    // be the first character in state.src, which is known since
+    // we have found an opening delimieter already.
+    start = state.pos + 2;
+    match = start;
+    while ( (match = state.src.indexOf("$$", match)) !== -1) {
+        // Found potential $$, look for escapes, pos will point to
+        // first non escape when complete
+        pos = match - 1;
+        while (state.src[pos] === "\\") { pos -= 1; }
+
+        // Even number of escapes, potential closing delimiter found
+        if ( ((match - pos) % 2) == 1 ) { break; }
+        match += 2;
+    }
+
+    // No closing delimter found.  Consume $$ and continue.
+    if (match === -1) {
+        if (!silent) { state.pending += "$$"; }
+        state.pos = start;
+        return true;
+    }
+
+    // Check if we have empty content, ie: $$$$.  Do not parse.
+    if (match - start === 0) {
+        if (!silent) { state.pending += "$$$$"; }
+        state.pos = start + 2;
+        return true;
+    }
+
+    // Check for valid closing delimiter
+    res = isValidDelim(state, match+1);
+    if (!res.can_close) {
+        if (!silent) { state.pending += "$$"; }
+        state.pos = start;
+        return true;
+    }
+
+    if (!silent) {
+        token         = state.push('math_block', 'math', 0);
+        token.block   = true;
+        token.markup  = "$$";
+        token.content = state.src.slice(start, match);
+    }
+
+    state.pos = match + 2;
+    return true;
+}
+
 function escapeHtml(unsafe) {
     return unsafe
         .replace(/&/g, "&amp;")
@@ -198,9 +260,11 @@ module.exports = function math_plugin(md, options) {
     }
 
     md.inline.ruler.after('escape', 'math_inline', math_inline);
+    md.inline.ruler.after('escape', 'math_inline_block', math_inline_block);
     md.block.ruler.after('blockquote', 'math_block', math_block, {
         alt: [ 'paragraph', 'reference', 'blockquote', 'list' ]
     });
     md.renderer.rules.math_inline = inlineRenderer;
+    md.renderer.rules.math_inline_block = blockRenderer;
     md.renderer.rules.math_block = blockRenderer;
 };
